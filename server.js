e0dec981f658496b670dcdf8a18994b4e1d85a08
Girish Madnani <girishmadnani05@gmail.com>
2022-04-03 19:07:53 +0100

    auth and helper functions

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Tech blog secret',
@@ -34,6 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({ force: false });
-  });
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
