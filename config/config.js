dc7f857c28a154a1ef28e601592ff4b643659eb5
Girish Madnani <girishmadnani05@gmail.com>
2022-04-03 19:15:41 +0100

    database fix

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
@@ -13,3 +13,4 @@ const sequelize = process.env.JAWSDB_URL
 
 
 module.exports = sequelize;
+
