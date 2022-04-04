26c650266f57de7e67f2b782dda193e9f797c5a1
Girish Madnani <girishmadnani05@gmail.com>
2022-04-04 16:11:29 +0100

    heroku fix

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`App listening on port ${process.env.PORT}!`);
   sequelize.sync({ force: false });
 });
